3ea52fd29a0edcf3f0ae95535cb528aadfd24241
Ivan vick <latorpromise@gmail.com>
2023-11-11 22:48:39 +0300

    full update

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['https://eshop-tutorial-pyri.vercel.app',],
+  origin: ['https://afia-guide1-0-hti6.vercel.app',],
   credentials: true
 }));
 
